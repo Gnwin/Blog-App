c480a60830bd5ce90ffc2ce4ac7897551fca52ce
Godwin Nwachukwu <nwgwin@gmail.com>
2021-06-01 10:58:33 +0100

    add changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def about
   end
+
+  def blog
+  end
+
+  def postdetails
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'about' => 'pages#about'
   root 'posts#index'
+  get 'about' => 'pages#about'
+  get 'blog' => 'pages#blog'
+  get 'postdetails' => 'pages#postdetails'
+  get 'contact' => 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
  
